51e568b645df02bbd6cdd79364a5caf02cf4111f
Kerry Russell <kdrussel@loyno.edu>
2018-01-12 16:07:54 -0600

    hero struct

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ import(
 	"github.com/gorilla/mux"
 )
 
+// Heroes Struct (Model)
+type Hero struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	Alias string `json:"alias"`
+	Birthday string `json:"birthday"`
+
+}
+
 func main() {
 	// Init router
 	r := mux.NewRouter()
@@ -17,7 +26,7 @@ func main() {
 	r.HandleFunc("/api/superheroes", getHeroes).Methods("GET")
 	r.HandleFunc("/api/superheroes/{id}", getHero).Methods("GET")
 	r.HandleFunc("/api/superheroes", createHero).Methods("POST")
-	r.HandleFunc("/api/superheroes/{id}", updateHeroes).Methods("PUT")
-	r.HandleFunc("/api/superheroes/{id}", deleteHeroes).Methods("DELETE")
-
+	r.HandleFunc("/api/superheroes/{id}", updateHero).Methods("PUT")
+	r.HandleFunc("/api/superheroes/{id}", deleteHero).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":3000". r))
 }
