4640c4b304d0dbbddfdad5e361979817df9bb7a6
Kerry Russell <kdrussel@loyno.edu>
2018-01-12 16:02:40 -0600

    routes

--- main.go
+++ main.go
@@ -12,4 +12,12 @@ import(
 func main() {
 	// Init router
 	r := mux.NewRouter()
+
+	// Route handlers / Endpoints
+	r.HandleFunc("/api/superheroes", getHeroes).Methods("GET")
+	r.HandleFunc("/api/superheroes/{id}", getHero).Methods("GET")
+	r.HandleFunc("/api/superheroes", createHero).Methods("POST")
+	r.HandleFunc("/api/superheroes/{id}", updateHeroes).Methods("PUT")
+	r.HandleFunc("/api/superheroes/{id}", deleteHeroes).Methods("DELETE")
+
 }
