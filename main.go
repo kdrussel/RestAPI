1e56acc8a67d766f71c848f7e4fd092f55964101
Kerry Russell <kdrussel@loyno.edu>
2018-01-12 19:01:50 -0600

    http functions, requests, syntax

--- main.go
+++ main.go
@@ -18,27 +18,45 @@ type Hero struct {
 
 }
 
-//Get all heroes
+// Init books var as a slice Hero struct
+var heroes []Hero
+
+// Get all heroes
 func getHeroes(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(heroes)
 }
 
-//Get single hero
+// Get single hero
 func getHero(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+	// Loop through books and find with id
+	for _, item := range heroes {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Hero{})
 }
 
-//Create hero
+// Create a new hero
 func createHero(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	var hero Hero
+	_ = json.NewDecoder(r.Body).Decode(&hero)
+	hero.ID = strconv.Itoa(rand.Intn(10000000)) // Mock ID
+	heroes = append(heroes, hero)
+	json.NewEncoder(w).Encode(hero)
 }
 
-//Update hero
+// Update hero
 func updateHero(w http.ResponseWriter, r *http.Request) {
 	
 }
 
-//Delete hero
+// Delete hero
 func deleteHero(w http.ResponseWriter, r *http.Request) {
 	
 }
@@ -47,11 +65,14 @@ func main() {
 	// Init router
 	r := mux.NewRouter()
 
+	// Mock data - add DB
+	heroes = append(heroes, Hero{ID: "1", Name:"Bruce Wayne", Alias: "Batman", Birthday: "Unknown"})
+
 	// Route handlers / Endpoints
 	r.HandleFunc("/api/superheroes", getHeroes).Methods("GET")
 	r.HandleFunc("/api/superheroes/{id}", getHero).Methods("GET")
 	r.HandleFunc("/api/superheroes", createHero).Methods("POST")
 	r.HandleFunc("/api/superheroes/{id}", updateHero).Methods("PUT")
 	r.HandleFunc("/api/superheroes/{id}", deleteHero).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":3000". r))
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
